ee187b8c11e433157899a2478b3421d3a32ebf40
Ann <ann.burd.9996@gmail.com>
2018-11-06 15:42:38 +0200

    Update inc/push_swap.h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -16,12 +16,25 @@
 #include "libft.h"
 #include <stdio.h> //printf
 
-typedef struct	s_stack
+typedef enum	e_st
 {
-	int 		*stack;
-	int			top;
+	a = 0,
+	b = 1,
+	both = 2
+}				t_st;
+
+#define A(x) stack->a[x]
+#define B(x) stack->b[x]
+#define TA stack->top[0] //top of A
+#define TB stack->top[1] //analogically
+
+typedef struct	s_ps
+{
+	int 		*a;
+	int 		*b;
+	int			top[2];
 	const int	size;
-}				t_stack;
+}				t_ps;
 
 // static int aa[4][3] = {
 //   {11, 12, 13},
@@ -48,9 +61,9 @@ void	push_swap();
 
 int		validate_nums_in_arg(char **arg);
 
-void	do_swap(t_stack stack);//ft_swap() ??
-void	do_push(t_stack stack[2], short s);
-void	do_rotate(t_stack stack);
-void	do_reverse_rotate(t_stack stack[2]);
+void	do_swap(t_ps *stack, t_st goal);//ft_swap() ??
+void	do_push(t_ps *stack, t_st goal);
+void	do_rotate(t_ps *stack, t_st goal);
+void	do_reverse_rotate(t_ps *stack, t_st goal);
 
 #endif
