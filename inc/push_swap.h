e7f21e79d9478792d1bfefb906c7b8750f88b897
Ann <ann.burd.9996@gmail.com>
2018-11-06 14:27:28 +0200

    Update inc/push_swap.h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -14,33 +14,43 @@
 #define PUSH_SWAP_H
 
 #include "libft.h"
-#include <stdio.h> //printf()
+#include <stdio.h> //printf
 
-typedef enum	e_st
+typedef struct	s_stack
 {
-	a,
-	b,
-	both
-}				t_st;
+	int 		*stack;
+	int			top;
+	const int	size;
+}				t_stack;
 
-typedef struct	s_tools
-{
-	int 		*a;
-	int 		*b;
-	const int	nums;
-}				t_tools;
+// static int aa[4][3] = {
+//   {11, 12, 13},
+//   {21, 22, 23},
+//   {31, 32, 33},
+//   {41, 42, 43}
+// };
+
+/*
+** checker
+*/
 
 void	checker();
 
+/*
+** push_swap
+*/
+
+void	push_swap();
+
+/*
+** common
+*/
+
 int		validate_nums_in_arg(char **arg);
 
-
-void	do_swap(t_tools stack, t_st which);
-void	do_push(t_tools stack, t_st which);
-void	do_rotate(t_tools stack, t_st which);
-void	do_reverse_rotate(t_tools stack, t_st which);
-
-
-
+void	do_swap(t_stack stack);//ft_swap() ??
+void	do_push(t_stack stack[2], short s);
+void	do_rotate(t_stack stack);
+void	do_reverse_rotate(t_stack stack[2]);
 
 #endif
